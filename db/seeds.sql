428bfb6a039586be117cfa5056793bb2fa0caf6e
Thomas Seaman <thomasseaman@Thomas-School-MacBook-Pro.local>
2018-04-24 21:59:52 -0700

    file create

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devour) VALUES ("Ruby's Diner Burger", false);
+INSERT INTO burgers (burger_name, devour) VALUES ("In-N-Out Burger", false);
+INSERT INTO burgers (burger_name, devour) VALUES ("A's Burger", false);
