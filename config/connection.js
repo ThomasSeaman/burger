6b7ee9647561bf5861d246e843a34b2dc84eb75f
Thomas Seaman <thomasseaman@Thomas-School-MacBook-Pro.local>
2018-04-29 12:20:20 -0700

    push

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,24 @@
 var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   port: 8889,
-//   host: "localhost",
-//   user: "root",
-//   password: "root",
-//   database: "burgers_db"
-// });
 var connection;
 
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'burgers_db',
-    root: 8889
-});
+    database: 'burgers_db'
+  });
+};
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+  // connection.connect(function(err) {
+  //   if (err) {
+  //     console.error("error connecting: " + err.stack);
+  //     return;
+  //   }
+  //   console.log("connected as id " + connection.threadId);
+  // });
 
-
-module.exports = connection;
+  connection.connect();
+  module.exports = connection;
